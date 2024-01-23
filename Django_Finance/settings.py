c5caada6104dbb8cada718e615d2521f6a45bfe2
3vil3y3 <101619460+evileye2002@users.noreply.github.com>
2024-01-23 16:33:39 +0700

    v0.1.9

--- Django_Finance/settings.py
+++ Django_Finance/settings.py
@@ -124,8 +124,8 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-STATICFILES_DIRS = [BASE_DIR / "static"]
-# STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
+# STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
