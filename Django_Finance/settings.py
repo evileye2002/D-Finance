305109b78e8e9699ac11e57b495a65157b879f97
3vil3y3 <101619460+evileye2002@users.noreply.github.com>
2024-01-23 16:07:45 +0700

    v0.1.4

--- Django_Finance/settings.py
+++ Django_Finance/settings.py
@@ -84,9 +84,8 @@ DATABASES = {
     }
 }
 
-DATABASE_URL = config("DATABASE_URL")
 
-DATABASES["default"] = dj_database_url.config(default=DATABASE_URL)
+DATABASES["default"] = dj_database_url.config(default=config("DATABASE_URL"))
 
 
 # Password validation
